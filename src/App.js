432d50771fa91a0fe9daebfdd0058b5104df9064
Akshay kumar singh <akshaysing975@gmail.com>
2024-09-12 19:03:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import MainPage from './components/Mainpage/Main';
+import PetServices from './components/Pet-Services/Services';
+import PetEssentials from './components/Pet-Essential/Essentials';
+import PetHealth from './components/Pet-Health/Health';
+import AboutUs from './components/About-Us/About';
+import Join from './components/Join/Join';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path="/Pet-Services" element={<PetServices />} />
+        <Route path="/pet-essentials" element={<PetEssentials />} />
+        <Route path="/pet-health" element={<PetHealth />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/join" element={<Join />} />
+      </Routes>
+    </Router>
   );
 }
 
